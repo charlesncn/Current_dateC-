0b95962163d2de688a8a8a03078a5a1f0caea4a5
Charles <53397655+charlesncn@users.noreply.github.com>
2021-11-05 17:50:01 +0300

    Create Current_dateC++.cxx

---
+++ Current_dateC++.cxx
@@ -0,0 +1,23 @@
+
+  
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+   // current date/time based on current system
+   time_t now = time(0);
+
+   cout << "Number of sec since January 1,1970:" << now << endl;
+
+   tm *ltm = localtime(&now);
+
+   // print various components of tm structure.
+   cout << "Year: " << 1900 + ltm->tm_year << endl;
+   cout << "Month: "<< 1 + ltm->tm_mon<< endl;
+   cout << "Day: "<<  ltm->tm_mday << endl;
+   cout << "Time: "<< 1 + ltm->tm_hour << ":";
+   cout << 1 + ltm->tm_min << ":";
+   cout << 1 + ltm->tm_sec << endl;
+}
